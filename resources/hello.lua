816c9341014bf520e55976edf9d6cfefb6e6b66a
root <root@DESKTOP-VJEK3S0>
2024-11-19 11:50:39 +0100

    modifeied interface to allow lua execution from js

--- resources/hello.lua
+++
@@ -1,4 +0,0 @@
-file = io.open("resources/myfile", "r")
-io.input(file)
-print(io.read())
-io.close(file)
--- src/luawasmmodule.c
+++ src/luawasmmodule.c
@@ -3,16 +3,30 @@
 #include "lualib.h"
 #include "lauxlib.h"
 
-void EMSCRIPTEN_KEEPALIVE sayHello() {
-    lua_State* L = luaL_newstate();
+static lua_State* L = NULL;
 
+void EMSCRIPTEN_KEEPALIVE createState() {
+    L = luaL_newstate();
     luaL_openlibs(L);
+}
 
-    if (luaL_loadfile(L, "resources/hello.lua") == LUA_OK) {
-        lua_call(L, 0, 0);
-    } else {
-	printf("Error opening hello.lua");
+int EMSCRIPTEN_KEEPALIVE executeLua(const char* lua) {
+    if (L == NULL) {
+	printf("Can't run lua before creating a state !\n");
+	return 1;
     }
 
+    if (luaL_dostring(L, lua) != LUA_OK) {
+        const char* error = lua_tostring(L, -1);
+	printf("Error executing lua: %s\n", error);
+	lua_pop(L, 1);
+	return 1;
+    }
+    
+    return 0;
+}
+
+void EMSCRIPTEN_KEEPALIVE closeState() {
     lua_close(L);
+    L = NULL;
 }
