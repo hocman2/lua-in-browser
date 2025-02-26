51633b48635ceb3c5f8377810ca3183deed125b7
Matthis <matthishoc@gmail.com>
2025-02-26 15:38:20 +0100

    fixed exports

--- node-pkg/src/index.ts
+++ node-pkg/src/index.ts
@@ -1,4 +1,4 @@
 export { LuaType } from "./lua-consts.js"
-export { LuaStateHandle, CodeHandle } from "./lua-interface.js"
+export { type LuaStateHandle, type CodeHandle, type LuaReference, type LuaFunction, type LuaExecutionError } from "./lua-interface.js"
 import {lua} from "./lua-interface.js"
 export default lua
